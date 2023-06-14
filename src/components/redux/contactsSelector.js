dec53959b324a5067d4153224b2faeff969de2a1
Vladislavv99 <vladlegur6@gmail.com>
2023-06-15 00:34:07 +0300

    .

--- src/components/redux/contactsSelector.js
+++
@@ -1,7 +0,0 @@
-export const getContacts = store => {
-  return store.contacts.filter(({ name }) =>
-    name.toLowerCase().includes(store.filtered.toLowerCase())
-  );
-};
-
-export const getFiltered = store => store.filtered;
